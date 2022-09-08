2c1958580a59489d864e66ef11f922d93e83344c
wanjiku <annritawanjiku77@gmail.com>
2022-09-08 12:15:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+      for (let i=0; i < array.length; i++)
+      {
+          //find complementary numbr that adds to our target
+          const complement = target - array[i]
+
+          //iterate over the remaining numbers in the array
+          for (let j=i+1; j<array.length; j++){
+            if (array[j] === complement) return true
+          }
+      }
+      return false
 }
 
 /* 
